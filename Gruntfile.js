338cd1f99be27dd67d42de25ed2b84ee3c1f3f77
vberecz <viktorberecz22@gmail.com>
2023-09-02 22:33:18 +0200

    Push the folder structure

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['./dist/**/*.css'],
+                dest: './dist/main.css'
+            }
+        },
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'scss/',
+                    src: ['main.scss'],
+                    dest: 'dist/',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['./scss/**/*.scss'],
+                tasks: ['sass', 'concat'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ["sass", "concat"]);
+
+};
